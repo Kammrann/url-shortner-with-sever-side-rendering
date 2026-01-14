b724046880192cd6a5aafc3cfbe472eef025780b
kammrann <kamranmka2234@gmail.com>
2026-01-14 11:22:40 +0530

    refactor: update authentication to use Bearer token from headers

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,13 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies.uid;
+  // const userUid = req.cookies.uid;
+  const userUid = req.headers["Authorization"];
 
   if (!userUid) return res.redirect("/login");
+  const token = userUid.split("Bearer ")[1];
 
-  const user = getUser(userUid);
+  const user = getUser(token);
 
   if (!user) return res.redirect("/login");
 
@@ -14,9 +16,11 @@ async function restrictToLoggedinUserOnly(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies.uid;
+  // const userUid = req.cookies.uid;
+  const userUid = req.headers["authorization"];
+  const token = userUid.split("Bearer ")[1]; 
 
-  const user = getUser(userUid);
+  const user = getUser(token);
 
   req.user = user;
   next();
